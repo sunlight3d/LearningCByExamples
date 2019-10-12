be80759cb8cbe9ba0d6fb44138f5985ee6ce6b5d
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-10-12 10:47:57 +0700

    update

--- 01/app01/app01/main.c
+++ 01/app01/app01/main.c
@@ -16,15 +16,38 @@ struct Book* input(){
     int i;
     for (i = 0; i < N; i++){
         INPUT_ONE: printf("Book number %d :\n", i+1);
-        printf("Enter book's name : \n");scanf("%s", (*(book+i)).name);
-        printf("Enter book's year : \n");scanf("%d", &((*(book+i)).year));
-        if((*(book+i)).year <= 2000 || (*(book+i)).price < 100 || (*(book+i)).price > 200 ) { //year must be > 2000, price >= 100, price <= 200,
+        printf("Enter book's name : ");scanf("%s", (*(book+i)).name);
+        printf("Enter book's year : ");scanf("%d", &((*(book+i)).year));
+        if((*(book+i)).year <= 2000)  {
+            printf("Year must be > 2000\n");
+            printf("Please re-enter your book's information\n");
             goto INPUT_ONE;
         }
-        printf("Enter book's publisher : \n");scanf("%s", (*(book+i)).publisher);
-        printf("Enter book's price : \n");scanf("%f", &(*(book+i)).price);
+        printf("Enter book's publisher :");scanf("%s", (*(book+i)).publisher);
+        printf("Enter book's price($) :");scanf("%f", &(*(book+i)).price);
+        if((*(book+i)).price < 100 ||
+            (*(book+i)).price > 200 ) {
+            printf("Price >= 100, price <= 200\n");
+            printf("Please re-enter your book's information\n");
+            goto INPUT_ONE;
+        }
     }
     return book;
+}
+
+void display(struct Book *book) {
+    int i;
+    printf("+----------------------------------------------+\n");
+    printf("|Name       |Year       |Publisher  |Price     |\n");
+    printf("+----------------------------------------------+\n");
+    for(i = 0; i < N && strcmp((book+i)->name, "") != 0; i++){
+        printf("|%-11s", (*(book+i)).name);
+        //printf("|%12s", book->name);
+        printf("|%-11d", (*(book+i)).year);
+        printf("|%-11s", (*(book+i)).publisher);
+        printf("|%-10.2f|\n", (*(book+i)).price);
+    }
+    printf("+----------------------------------------------+\n");
 }
 void sort(struct Book* book){
     int i, j;
@@ -39,23 +62,52 @@ void sort(struct Book* book){
             }
         }
     }
-    printf("Sort finished\n");
+    display(book);
 }
-void display(struct Book *book) {
-    int i;
-    for(i = 0; i < N; i++){
-        printf("Book's name : %s\n", (*(book+i)).name);
-        printf("Book's year : %d\n", (*(book+i)).year);
-        printf("Book's publisher : %s\n", (*(book+i)).publisher);
+void analyze(struct Book *book){
+    int i, j;
+    char publishers[N][150];//Create an array which contains "publishers"
+    for (i = 0; i < N; i++) {
+        strcpy(publishers[i], (*(book+i)).publisher);
+    }
+    //sort ascending
+    for (i = 0; i < N-1; i++) {
+        for (j = i+1; j < N; j++) {
+            if(strcmp(publishers[i], publishers[j]) > 0) {
+                char temp[150];
+                strcpy(temp, publishers[i]);
+                strcpy(publishers[i], publishers[j]);
+                strcpy(publishers[j], temp);
+            }
+        }
+    }
+    printf("Statistics Result:\n");
+    char publisherName[150];
+    int count = 0;
+    for (i = 0; i < N; i++) {
+        if(i == 0) {
+            strcpy(publisherName, publishers[i]);
+            count++;
+        } else if(strcmp(publishers[i], publisherName) != 0){
+            printf("+There are %d Book(s) published by %s\n", count, publisherName);
+            strcpy(publisherName, publishers[i]);
+            count = 1;
+        } else if(strcmp(publishers[i], publisherName) == 0) {
+            count++;
+        }
+    }
+    if(N > 0) {
+        printf("+There are %d Book(s) published by %s\n", count, publisherName);
     }
 }
 void find(struct Book *book) {
     printf("Find some books \n");
-    struct Book *bookResults = (struct Book *)malloc(sizeof(struct Book));
+    struct Book *bookResults = (struct Book *)malloc(N * sizeof(struct Book));
     struct Book *ptr;
     int i = 0;
     float maxPrice, minPrice;
     char publisher[200];
+    
     printf("Book's publisher: "); scanf("%s", publisher);
     printf("Enter min price: "); scanf("%f", &minPrice);
     printf("Enter max price: "); scanf("%f", &maxPrice);
@@ -64,18 +116,13 @@ void find(struct Book *book) {
     for(i = 0; i < N; i++){
         if(strcmp((*(book+i)).publisher, publisher) == 0 &&
            (*(book+i)).price >= minPrice && (*(book+i)).price <= maxPrice ){
-            ptr = (struct Book *)malloc(sizeof(struct Book));
+            *ptr = *(book+i);
             ptr++;
             j++;
         }
     }
     printf("Search result : \n");
-    for(i = 0; i < j; i++){
-        printf("Book's name : %s\n", (*(book+i)).name);
-        printf("Book's year : %d\n", (*(book+i)).year);
-        printf("Book's publisher : %s\n", (*(book+i)).publisher);
-        printf("Book's price : %f\n", (*(book+i)).price);
-    }
+    display(bookResults);
 }
 void save(struct Book *book){
     char fileName[100];
@@ -99,8 +146,8 @@ void openFile(struct Book *book) {
     char fileName[100];
     char filePath[250];
     printf("Enter file name to open : "); scanf("%s", fileName);
-    //strcat(filePath, "/Users/hoangnd/Documents/tutorials/AptechPTTH/"); //MacOS+Linux
-    strcat(filePath, "D:\\");//Windows
+    strcat(filePath, "/Users/hoangnd/Documents/tutorials/AptechPTTH/"); //MacOS+Linux
+    //strcat(filePath, "D:\\");//Windows
     strcat(filePath, fileName);
     FILE *file = fopen(filePath,"rb"); // w for write, b for binary
     
@@ -113,14 +160,16 @@ void openFile(struct Book *book) {
     fclose(file);
 }
 int main(int argc, char * argv[]) {
+    struct Book *book = NULL;
+    char choice;
+    
+    LABEL1:
     printf("+-------------------------------------------------------------------------+\n");
     printf("|                         BOOK MANAGEMENT                                 |\n");
     printf("+-------------------------------------------------------------------------+\n");
     printf("+ 1.Input books | 2.Sort | 3.Analyze | 4.Find. | 5.Save | 6.Open | 7.Exit +\n");
     printf("+-------------------------------------------------------------------------+\n");
-    struct Book *book = NULL;
-    char choice;
-    LABEL1: printf("Enter your choice : "); choice = getchar();
+    printf("Enter your choice : "); choice = getchar();
     while (choice == '\n') {
         choice = getchar();
     }
@@ -134,7 +183,7 @@ int main(int argc, char * argv[]) {
             goto LABEL2;
             break;
         case '3':
-            display(book);
+            analyze(book);
             goto LABEL2;
             break;
         case '4':
@@ -142,10 +191,12 @@ int main(int argc, char * argv[]) {
             goto LABEL2;
             break;
         case '5':
-            return 0;
+            save(book);
+            goto LABEL2;
             break;
         case '6':
-            
+            save(book);
+            goto LABEL2;
             break;
         case '7':
             return 0;
@@ -177,8 +228,10 @@ int main(int argc, char * argv[]) {
             //clear screen
             system("clear");//Linux+MacOS
             //system("cls");//Windows
+            goto LABEL1;
             break;
         default:
+            goto LABEL2;
             break;
     }
     return 0;
